e6cdc85a5efb42a79c1fc64d92d64f72a973a5b5
Guillermo Salas Serey <guillermo.salas@proactivess.cl>
2022-09-21 01:05:19 -0300

    feat: add new routes for comment

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('/comments', 'CommentsController@index');
 Route::post('/comments', 'CommentsController@new');
 Route::post('/comments/{id}', 'CommentsController@reply');
+Route::put('/comments/{id}', 'CommentsController@update');
+Route::delete('/comments/{id}', 'CommentsController@delete');
